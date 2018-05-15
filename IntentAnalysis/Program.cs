f7bf4f29076564c5848bb780186a0e5f6e2dab79
Sparks, Collin <collin.sparks@aarons.com>
2018-05-15 10:18:13 -0400

    Add project files.

---
+++ IntentAnalysis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntentAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
